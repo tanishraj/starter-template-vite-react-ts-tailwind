e7faed85303cad0735a29f6bc55d6ab5e4336393
Tanish Raj <learn.tanishraj@gmail.com>
2024-10-25 01:11:49 +0400

    fix: SVG as React component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { HelloWorld } from '@/components/HelloWorld';
-import viteLogo from '@/assets/logo/vite.svg';
-import reactLogo from '@/assets/logo/react.svg';
+import ViteLogo from '@/assets/logo/vite.svg';
+import ReactLogo from '@/assets/logo/react.svg';
 import './App.scss';
 
 export const App = () => {
@@ -8,10 +8,12 @@ export const App = () => {
     <div className='App'>
       <div>
         <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
+          <ViteLogo />
+          {/* <img src={ViteLogo} className='logo' alt='Vite logo' /> */}
         </a>
         <a href='https://vuejs.org/' target='_blank'>
-          <img src={reactLogo} className='logo vue' alt='Vue logo' />
+          <ReactLogo />
+          {/* <img src={reactLogo} className='logo vue' alt='Vue logo' /> */}
         </a>
       </div>
       <HelloWorld msg='Vite + React' />
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    svgr({
+      svgrOptions: {
+        exportType: 'default',
+        ref: true,
+        svgo: false,
+        titleProp: true
+      },
+      include: '**/*.svg'
+    })
+  ],
   server: {
     port: 3000
   },
