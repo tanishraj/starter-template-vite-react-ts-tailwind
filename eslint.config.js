3947721fa3b0a526febedaa1f8365a3e39918c7c
Tanish Raj <learn.tanishraj@gmail.com>
2024-10-27 17:21:51 +0400

    feat: basic eslint config with prettier

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginJsxA11y from 'eslint-plugin-jsx-a11y';
+import pluginImport from 'eslint-plugin-import';
+import pluginPrettier from 'eslint-plugin-prettier';
+import configPrettier from 'eslint-config-prettier';
+import react from 'eslint-plugin-import/config/react';
+
+export default [
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  configPrettier,
+  {
+    files: ['**/*.{js,ts,jsx,tsx}'],
+    ignores: ['node_modules', 'dist', 'build'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    plugins: {
+      react: pluginReact,
+      'react-hooks': pluginReactHooks,
+      'jsx-a11y': pluginJsxA11y,
+      import: pluginImport,
+      perttier: pluginPrettier,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
