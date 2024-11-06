3920f091b7ec742703523e6b752092e53de9bfc8
Tanish Raj <learn.tanishraj@gmail.com>
2024-11-06 22:01:18 +0530

    feat: adding react-router-dom package

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import { App } from '@/App.js';
 import './i18n';
 
 import './main.css';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
